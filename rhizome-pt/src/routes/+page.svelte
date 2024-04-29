da72feaf490a2fcac29006d11e7dcbb9754bdafa
scmacoll <85879687+scmacoll@users.noreply.github.com>
2024-04-29 16:20:28 +1000

    edited colors

--- rhizome-pt/src/routes/+page.svelte
+++ rhizome-pt/src/routes/+page.svelte
@@ -4,36 +4,36 @@
   <table class="text-xs table-auto table-xs w-fit">
     <!-- head -->
     <thead>
-    <tr class="border-black text-center uppercase">
-      <th class="border-y border-r border-black">_|</th>
-      <th class="border-y border-r border-black">A</th>
-      <th class="border-y border-r border-black">B</th>
-      <th class="border-y border-r border-black">C</th>
-      <th class="w-6 border-white border-primary border-r"></th>
+    <tr class="border-opacity-30 border-base-content text-center uppercase text-neutral-content">
+      <th class="bg-neutral border-y border-r border-opacity-30 border-base-content">_|</th>
+      <th class="bg-neutral border-y border-r border-opacity-30 border-base-content ">A</th>
+      <th class="bg-neutral border-y border-r border-opacity-30 border-base-content ">B</th>
+      <th class="bg-neutral border-y border-r border-opacity-30 border-base-content ">C</th>
+      <th class="w-6 border-r border-base-content border-opacity-70"></th>
       <th class="w-6"></th>
-      <th class="border-y border-x border-black">A</th>
+      <th class="bg-neutral border-y border-x border-opacity-30 border-base-content ">A</th>
     </tr>
     </thead>
     <tbody>
     <!-- row 1 -->
     <tr class="font-bold text-center">
-      <th class="text-start font-normal text-xs border-r border-b border-black">N4J</th>
-      <td class="border-r border-b border-black">Label</td>
-      <td class="border-r border-b border-black">Properties</td>
-      <td class="border-r border-b border-black">Relationship</td>
-      <th class="w-6 border-white border-primary border-r"></th>
+      <th class="text-start font-normal text-xs border-r border-b border-opacity-30 border-base-content ">N4J</th>
+      <td class=" border-r border-b border-opacity-30 border-base-content ">Label</td>
+      <td class="border-r border-b border-opacity-30 border-base-content ">Properties</td>
+      <td class="border-r border-b border-opacity-30 border-base-content ">Relationship</td>
+      <th class="w-6 border-r border-base-content border-opacity-70"></th>
       <th class="w-6"></th>
-      <td class="border-x border-b border-black">Cypher</td>
+      <td class="border-x border-b border-opacity-30 border-base-content ">Cypher</td>
     </tr>
     <!-- row 1 -->
-    <tr class="">
-      <th class="bg-base-200 text-start border-r border-b border-black">1</th>
-      <td class="bg-base-200 border-r border-b border-black">Cy Ganderton</td>
-      <td class="bg-base-200 border-r border-b border-black">Quality Control Specialist</td>
-      <td class="bg-base-200 border-r border-b border-black">Blue</td>
-      <th class="w-6 border-white border-primary border-r"></th>
+    <tr class="text-neutral-content">
+      <th class="bg-neutral text-start border-r border-b border-opacity-30 border-base-content">1</th>
+      <td class="bg-neutral border-r border-b border-opacity-30 border-base-content hover:bg-gray-500 hover:bg-opacity-20">Cy Ganderton</td>
+      <td class="bg-neutral border-r border-b border-opacity-30 border-base-content hover:bg-gray-500 hover:bg-opacity-20">Quality Control Specialist</td>
+      <td class="bg-neutral border-r border-b border-opacity-30 border-base-content hover:bg-gray-500 hover:bg-opacity-20">Blue</td>
+      <th class="w-6 border-r border-base-content border-opacity-70"></th>
       <th class="w-6"></th>
-      <td class="bg-base-200 border-x border-b border-black text-center">
+      <td class="bg-neutral border-x border-b border-opacity-30 border-base-content text-center hover:bg-gray-500 hover:bg-opacity-20">
         CREATE (john:Person &123;name: 'John', age: 30&125;)
         CREATE (alice:Person &123;name: 'Alice', age: 25&125;)
         CREATE (john)-[:KNOWS]->(alice)
@@ -42,24 +42,24 @@
     </tr>
     <!-- row 2 -->
     <tr class="">
-      <th class="text-start border-r border-b border-black">2</th>
-      <td class="border-r border-b border-black">Hart Hagerty</td>
-      <td class="border-r border-b border-black">Desktop Support Technician</td>
-      <td class="border-r border-b border-black">Purple</td>
-      <th class="w-6 border-white border-primary border-r"></th>
+      <th class="text-start border-r border-b border-opacity-30 border-base-content">2</th>
+      <td class="border-r border-b border-opacity-30 border-base-content hover:bg-gray-500 hover:bg-opacity-20">Hart Hagerty</td>
+      <td class="border-r border-b border-opacity-30 border-base-content hover:bg-gray-500 hover:bg-opacity-20">Desktop Support Technician</td>
+      <td class="border-r border-b border-opacity-30 border-base-content hover:bg-gray-500 hover:bg-opacity-20">Purple</td>
+      <th class="w-6 border-r border-base-content border-opacity-70"></th>
       <th class="w-6"></th>
-      <td class="border-x border-b border-black text-center opacity-0">
+      <td class="border-x border-b border-opacity-30 border-base-content text-center opacity-0">
       </td>
     </tr>
     <!-- row 3 -->
-    <tr class="">
-      <th class="bg-base-200 text-start border-r border-b border-black">3</th>
-      <td class="bg-base-200 border-r border-b border-black">Brice Swyre</td>
-      <td class="bg-base-200 border-r border-b border-black">Tax Accountant</td>
-      <td class="bg-base-200 border-r border-b border-black">Red</td>
-      <th class="w-6 border-white border-primary border-r"></th>
+    <tr class="text-neutral-content">
+      <th class="bg-neutral text-start border-r border-b border-opacity-30 border-base-content">3</th>
+      <td class="bg-neutral border-r border-b border-opacity-30 border-base-content hover:bg-gray-500 hover:bg-opacity-20">Brice Swyre</td>
+      <td class="bg-neutral border-r border-b border-opacity-30 border-base-content hover:bg-gray-500 hover:bg-opacity-20">Tax Accountant</td>
+      <td class="bg-neutral border-r border-b border-opacity-30 border-base-content hover:bg-gray-500 hover:bg-opacity-20">Red</td>
+      <th class="w-6 border-r border-base-content border-opacity-70"></th>
       <th class="w-6"></th>
-      <td class="bg-base-200 border-x border-b border-black text-center">
+      <td class="bg-neutral border-x border-b border-opacity-30 border-base-content text-center">
         <p class="opacity-0"></p>
       </td>
     </tr>
--- rhizome-pt/tailwind.config.js
+++ rhizome-pt/tailwind.config.js
@@ -5,5 +5,15 @@ export default {
     extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: true,
+    darkTheme: "dim",
+    base: true,
+    styled: true,
+    utils: true,
+    prefix: "",
+    log: true,
+    themeRoot: ":root",
+  },
 }
 
